1ea84a913d324bed28babc4136fa7d4dcf389e4a
Pandiarajan S <pandiarajans@outlook.com>
2023-08-06 06:11:20 +0530

    disabling nullable property in api project

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -7,6 +7,6 @@ namespace API.Entities
     {
         [Key]
         public int Id { get; set; }
-        public string? UserName { get; set; }
+        public string UserName { get; set; }
     }
 }
